87d6af918726a7e71a369cca01ac9f4b33a94019
luxingzhi27 <3523718637@qq.com>
2025-11-26 03:26:43 +0800

    random.sample()bug修改

--- DyGPrompt/utils/data_processing.py
+++ DyGPrompt/utils/data_processing.py
@@ -19,8 +19,8 @@ def get_data_node_classification(dataset_name, use_validation=False):
   ### Load data and train val test split
   graph_df = pd.read_csv('./downstream_data/{}/ds_{}.csv'.format(dataset_name,dataset_name))
   edge_features = np.load('./processed/ml_{}.npy'.format(dataset_name))
-  node_features = np.load('./processed/ml_{}_node.npy'.format(dataset_name)) 
-    
+  node_features = np.load('./processed/ml_{}_node.npy'.format(dataset_name))
+
 
   val_time, test_time = list(np.quantile(graph_df.ts, [0.40, 0.45]))
 
@@ -54,8 +54,8 @@ def get_dd_data(dataset_name, different_new_nodes_between_val_and_test=False, ra
   ### Load data and train val test split
   graph_df = pd.read_csv('./downstream_data/{}/ds_{}.csv'.format(dataset_name,dataset_name))
   edge_features = np.load('./processed/ml_{}.npy'.format(dataset_name))
-  node_features = np.load('./processed/ml_{}_node.npy'.format(dataset_name)) 
-    
+  node_features = np.load('./processed/ml_{}_node.npy'.format(dataset_name))
+
   if randomize_features:
     node_features = np.random.rand(node_features.shape[0], node_features.shape[1])
 
@@ -79,7 +79,7 @@ def get_dd_data(dataset_name, different_new_nodes_between_val_and_test=False, ra
     set(destinations[timestamps > val_time]))
   # Sample nodes which we keep as new nodes (to test inductiveness), so than we have to remove all
   # their edges from training
-  new_test_node_set = set(random.sample(test_node_set, int(0.1 * n_total_unique_nodes)))
+  new_test_node_set = set(random.sample(sorted(test_node_set), int(0.1 * n_total_unique_nodes)))
 
   # Mask saying for each source and destination whether they are new test nodes
   new_test_source_mask = graph_df.u.map(lambda x: x in new_test_node_set).values
@@ -160,8 +160,8 @@ def get_data(dataset_name, different_new_nodes_between_val_and_test=False, rando
   ### Load data and train val test split
   graph_df = pd.read_csv('./processed/ml_{}.csv'.format(dataset_name))
   edge_features = np.load('./processed/ml_{}.npy'.format(dataset_name))
-  node_features = np.load('./processed/ml_{}_node.npy'.format(dataset_name)) 
-    
+  node_features = np.load('./processed/ml_{}_node.npy'.format(dataset_name))
+
   if randomize_features:
     node_features = np.random.rand(node_features.shape[0], node_features.shape[1])
 
@@ -185,7 +185,7 @@ def get_data(dataset_name, different_new_nodes_between_val_and_test=False, rando
     set(destinations[timestamps > val_time]))
   # Sample nodes which we keep as new nodes (to test inductiveness), so than we have to remove all
   # their edges from training
-  new_test_node_set = set(random.sample(test_node_set, int(0.1 * n_total_unique_nodes)))
+  new_test_node_set = set(random.sample(sorted(test_node_set), int(0.1 * n_total_unique_nodes)))
 
   # Mask saying for each source and destination whether they are new test nodes
   new_test_source_mask = graph_df.u.map(lambda x: x in new_test_node_set).values
@@ -266,8 +266,8 @@ def get_d_data(dataset_name, different_new_nodes_between_val_and_test=False, ran
   ### Load data and train val test split
   graph_df = pd.read_csv('./downstream_data/{}/ds_{}.csv'.format(dataset_name,dataset_name))
   edge_features = np.load('./processed/ml_{}.npy'.format(dataset_name))
-  node_features = np.load('./processed/ml_{}_node.npy'.format(dataset_name)) 
-    
+  node_features = np.load('./processed/ml_{}_node.npy'.format(dataset_name))
+
   if randomize_features:
     node_features = np.random.rand(node_features.shape[0], node_features.shape[1])
 
@@ -291,7 +291,7 @@ def get_d_data(dataset_name, different_new_nodes_between_val_and_test=False, ran
     set(destinations[timestamps > val_time]))
   # Sample nodes which we keep as new nodes (to test inductiveness), so than we have to remove all
   # their edges from training
-  new_test_node_set = set(random.sample(test_node_set, int(0.1 * n_total_unique_nodes)))
+  new_test_node_set = set(random.sample(sorted(test_node_set), int(0.1 * n_total_unique_nodes)))
 
   # Mask saying for each source and destination whether they are new test nodes
   new_test_source_mask = graph_df.u.map(lambda x: x in new_test_node_set).values
